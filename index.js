c0b0efb3f274bbe3a77613e46493ff0b914b5eb9
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-10 23:52:01 +0600

    complete

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run(){
             res.send(review);
         });
 
-        app.delete('totalReviews/:id', async(req, res)=>{
+        app.delete('/totalReviews/:id', async(req, res)=>{
             const id = req.params.id;
             const query = {_id : ObjectId(id) };
             const result = await reviewsCollection.deleteOne(query);
